bc0f398558ce786b41e65cb5729a17f330ebabe4
billferro <bferro@charter.net>
2015-03-21 13:38:31 -0400

    updating documentation

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # GetData012Project
 Project for Johns Hopkins' Getting and Cleaning Data Coursera course
+## Source Data
+
+The data used for this assignment is based on a research study involving smartphones and human activity[1].  Description of the study 
+can be found [here](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones).  The raw dataset used by the project
+can be found [here](https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip).
+
+This project assumes that you have downloaded the raw data set and unzipped it into a subfolder named "UCI HAR Dataset" in your current working directory.
+
+## Project Files
+
+* `run_analysis.R` - Script that performs the project steps required to combine the initial raw data sets and produce the resulting tidy data set.
+####Requirements
+The script must meet the following requirements:
+	1. Merges the training and the test sets to create one data set.
+	2. Extracts only the measurements on the mean and standard deviation for each measurement.
+	3. Uses descriptive activity names to name the activities in the data set.
+	4. Appropriately labels the data set with descriptive variable names.
+	5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+####Tidy Data Set
+The resulting output txt file meets the principals of a tidy data set because
+<ol>
+	<li>Each measured variable is in one column as noted by the xxMean and xxStd columns in the resulting file.</li>
+	<li>Each obvservation is on a different row as noted by the 180 rows representing 30 subjects and 6 activities for each subject.</li>
+	<li>There is one table for each kind of variable.  In this case, there is only one table and it represents the calculated averages of the subject/activity combinations.</li>
+</ol>
+* `features.txt` - List of all of the features in the original data set for which we have calculated the averages
+
+* `CodeBook.md` - Description of the columns in the output data set, TidyDataSet.txt
+
+* `README.md` - This file
+
+
+## Output File
+
+TidyDataSet.txt - See `CodeBook.md` for a description of the data.
+
+
+## Acknowledgements
+The raw data set is part of the following research study
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
--- run_analysis.R
+++ run_analysis.R
@@ -1,50 +1,91 @@
 # run_analysis.R
 # Author: Bill Ferro
-
-#1. Merge the training and the test sets to create one data set.
-
-# Read in the activities list
-activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
-# Read in the features list
-features <- read.table("./UCI HAR Dataset/features.txt")
+# Overview:
+#  The goal of this project is to 
+#    1. Merge two data sets from the UCI HAR Data Set at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+#    2. Extract only the mean and standard deviation measurements from both data sets
+#    3. Use descriptive activity names to name the activities in the data set
+#    4. From the combined data set, create a new tidy data set with the
+#       average of each variable for each activity and each subject combination
+# Assumptions:
+#  1. The source data set has been downloaded and unzipped into a folder
+#     named "UCI HAR Dataset" in the current working directory
+# Full description of the project and output data set are in the README.md and 
+# CodeBook.md files
 
 #
-# Read in the Test data Set
-# Read in the Training data set
-# Merge(concatenate) the 3 contents together
+# Load in data definitions for use later in the script
+#
+#     Read in the activities list to convert the activity numbers to names
+#     Read in the features list for the input data set column labels
+#     Read in the features list for the output data set column labels
+#
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+tds_features <- read.table("./features.txt")
+
+#
+# Load the first data set: TEST
+#   1. Read in the user ID of the subject for each entry in the data set
+#   2. Read in the Activity that the user was performing for each entry in the data set
+#   3. Read in the test data set setting the column names to features list loaded above
+#
 subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.names=c("Subject"))
-x_test <- read.table("./UCI HAR Dataset/test/x_test.txt", col.names=features[,2])
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names=c("Activity"))
+x_test <- read.table("./UCI HAR Dataset/test/x_test.txt", col.names=features[,2])
 
+# Load the second data set: TRAIN
+#   1. Read in the user ID of the subject for each entry in the data set
+#   2. Read in the Activity that the user was performing for each entry in the data set
+#   3. Read in the test data set setting the column names to features list loaded above
+#
 subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.names=c("Subject"))
-x_train <- read.table("./UCI HAR Dataset/train/x_train.txt", col.names=features[,2])
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names=c("Activity"))
+x_train <- read.table("./UCI HAR Dataset/train/x_train.txt", col.names=features[,2])
 
+#
+# Merge the two data sets together by creating new data frames with the combined parts
+#   (Part 1 of the assignment)
+#
 subject <- rbind(subject_test, subject_train)
 x <- rbind(x_test, x_train)
 y <- rbind(y_test, y_train)
 
-#2. Extract only the measurements on the mean and standard deviation 
-# for each measurement. 
+#
+# Extract only the measurements on the mean and standard deviation for each measurement. 
 # Filter out the columns based on the appearance of "mean" or "std" in the current labels
-cols <- grep(".*mean|.*std", features$V2, ignore.case=TRUE)
-# combine the 3 data sets as we extract
+# ignoring the angle... definitions
+# Combine the Subject, Activity and required columns into a new data set (ds)
+#    (Part 2 of the assignment)
+#
+cols <- grep("[tf].*mean\\(|[tf].*std\\(", features$V2, ignore.case=TRUE)
 ds <- cbind(subject, y, x[,cols])
 
-#3. Use descriptive activity names to name the activities in the data set
-# Get the factors as labels
+#
+# Use descriptive activity names to name the activities in the data set
+# by first getting the factors as labels from the activity list loaded above
+# and then replacing the Activity column in the data set with the new labels
+#    (Part 3 of the assignment)
+#
 f <- factor(ds$Activity, levels=activities[,1], labels=activities[,2])
-# Replace the Activity column with labels
 ds$Activity <- f
 
-#4. Appropriately labels the data set with descriptive variable names.
-# did this at the Read Table level
-#TODO: Change the names to something more descriptive
-#  by loading them up from a txt file
-
-#5. From the data set in step 4, creates a second, independent tidy 
-# data set with the average of each variable for each activity and each subject.
+#
+# For each Subject/Activity combination in the data set, calculate
+# the average.  Create a new, tidy data set with the average for each
+# activity and subject combination and label each column appropriately
+#
+# Note: numcolwise takes advantage of the fact that the data set contains
+# numeric data types for every column that we care about and the other columns
+# are int or factor data types
+#    (Part 4 of the assignment)
+#
 library(plyr)
 tidyDS <- ddply(ds, .(Subject, Activity), numcolwise(mean))
+names(tidyDS) <- tds_features[,1]
 
+#
+# Output the tidy data set for review.  You can read this back in using
+#  read.table("TidyDataSet.txt", header=TRUE)
+#
 write.table(tidyDS, file="TidyDataSet.txt", row.name=FALSE)
